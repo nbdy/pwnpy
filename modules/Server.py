9228e3c8ad5c2b329ed4f7798e06683f0595c0ba
smthnspcl <pascal@eberlein.io>
2019-12-31 21:30:06 +0100

    update

--- modules/Server.py
+++ modules/Server.py
@@ -1,4 +1,4 @@
-from sanic import Sanic, response
+from flask import Flask
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from os import getcwd
 from libs import IThread
@@ -9,6 +9,9 @@ from libs import IThread
 class Server(IThread):
     app = None
     env = None
+
+    dir_tpl = None
+    dir_static = None
 
     @staticmethod
     def parse_parameters(data):
@@ -26,66 +29,66 @@ class Server(IThread):
         return s
 
     def _on_run(self):
-        tpl_dir = getcwd() + "/" + self.cfg["templates"]
-        tpl_dir = self.endswith_append(tpl_dir, "/")
+        self.dir_tpl = getcwd() + "/" + self.cfg["templates"]
+        self.dir_tpl = self.endswith_append(self.dir_tpl, "/")
+
         self.env = Environment(
-            loader=FileSystemLoader(tpl_dir),
+            loader=FileSystemLoader(self.dir_tpl),
             autoescape=select_autoescape(["html"])
         )
 
-        self.app = Sanic(self.name)
-        static_dir = getcwd() + "/" + self.cfg["static"]
-        static_dir = self.endswith_append(static_dir, "/")
+        self.dir_static = getcwd() + "/" + self.cfg["static"]
+        self.dir_static = self.endswith_append(self.dir_static, "/")
 
-        self.app.static('static', static_dir)
-
-        def dashboard():
-            p = self.db.current_position()
-            if p is None:
-                p = self.cfg["defaultPosition"]
-            else:
-                p = [p[1], p[0]]
-            r = {
-                "counts": self.db.counts(),
-                "currentPosition": p
-            }
-            print(r)
-            return response.html(self.env.get_template("dashboard.html").render(r))
+        self.app = Flask(self.name)
 
         @self.app.route("/")
-        async def root(req):
-            return dashboard()
+        def root():
+            return self.dashboard()
 
-        @self.app.route("/api/columns/<path:path>")
-        async def api_column_names(req, path):
-            return response.json(self.db.get_columns(path))
+        @self.app.route("/dashboard")
+        def dashboard():
+            return self.dashboard()
 
-        @self.app.route("/api/positions/<path:path>")
-        async def api_positions(path):
-            return "positions"
+    def dashboard(self):
+        p = self.db.current_position()
+        if p is None:
+            p = self.cfg["defaultPosition"]
+        else:
+            p = [p[1], p[0]]
+        r = {
+            "counts": self.db.counts(),
+            "currentPosition": p
+        }
+        print(r)
+        return self.env.get_template("dashboard.html").render(r)
 
-        @self.app.route("/api/search", methods=["POST"])
-        def api_search(req):
-            print(req.query_string)
-            return response.json({"yuh": "skrrt"})  # todo
+    '''
+    def api_column_names(self, req, path):
+        return "skrt"
 
-        @self.app.websocket("/api/counts")
-        async def api_counts(req, ws):
-            lc = None
-            while True:
-                nc = self.db.counts()
-                if nc != lc:
-                    await ws.send(nc)
-                    lc = nc
+    async def api_positions(self, path):
+        return "positions"
+
+    def api_search(self, req):
+        print(req.query_string)
+        return "yuh"
+
+    async def api_counts(self, req, ws):
+        lc = None
+        while True:
+            nc = self.db.counts()
+            if nc != lc:
+                await ws.send(nc)
+                lc = nc
+    '''
 
     def run(self):
         if not self.do_run:
             return
         self._on_run()
-        self.log_info("listening on: '%s:%i'" % (self.cfg["host"], self.cfg["port"]))
-        self.app.run(self.cfg["host"],
-                     self.cfg["port"],
-                     threaded=self.cfg["threaded"],
-                     use_reloader=False,
-                     debug=True,
-                     distributed=True)
+        self.app.run(
+            self.cfg["host"],
+            self.cfg["port"],
+            threaded=self.cfg["threaded"]
+        )
