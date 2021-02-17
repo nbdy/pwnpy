d72a121fa6ae0bb5393d54fa6083048fdfcb2570
Pascal Eberlein <pascal@eberlein.io>
2021-02-17 22:08:25 +0100

    update

--- pwnpy/modules/UI.py
+++ pwnpy/modules/UI.py
@@ -334,7 +334,7 @@ class UI(Module):
         y = 2
         db.text((x, y), 'pwnpy', font=self.font, fill=0)
         for key in data.keys():
-            x += 80
+            y += 20
             line = "{}: ".format(key)
             for sk in data[key].keys():
                 line += "{}: {} | ".format(sk, data[key][sk])
