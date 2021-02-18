8ecbd3ce7956f23ccdc404f087853f7cde9a8e88
Pascal Eberlein <pascal@eberlein.io>
2021-02-18 23:11:02 +0100

    update

--- pwnpy/modules/BT.py
+++ pwnpy/modules/BT.py
@@ -1,18 +1,19 @@
-from pwnpy import Module, is_root
-from podb import DB
+from pwnpy import Module, is_root, Manager
 from btpy import LEDevice, ClassicDevice
+from podb import DBEntry
 from loguru import logger as log
 from time import sleep
 
 
 class BT(Module):
-    def __init__(self, db: DB, **kwargs):
-        Module.__init__(self, "BT", db)
+    def __init__(self, mgr: Manager, **kwargs):
+        Module.__init__(self, "BT", mgr)
         self.device = kwargs.get("device") or "hci0"
         self.devs_types = [ClassicDevice]
         if is_root():
-            log.warning("Disabling BT LE scanning because we do not have root rights.")
             self.devs_types.append(LEDevice)
+        else:
+            log.warning("Disabling BT LE scanning because we do not have root rights.")
 
     def work(self):
         for dt in self.devs_types:
@@ -20,4 +21,7 @@ class BT(Module):
             for device in devices:
                 if "GPS" in self.mgr.shared_data:
                     device.__dict__.update(self.mgr.shared_data["GPS"])
+                entry = DBEntry(**device.__dict__)
+                log.debug("Found: {} | {}", device.name, device.address)
+                self.mgr.db.upsert(entry)
         sleep(0.2)
