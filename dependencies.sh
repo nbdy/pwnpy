60071be91963078c6678e386b2be32b6785540ca
Pascal Eberlein <pascal@eberlein.io>
2021-02-23 01:17:29 +0100

    update

--- dependencies.sh
+++ dependencies.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get install -y python3 python3-dev python3-pip gpsd gpsd-clients libgps-dev python-gps libopenjp2-tools aircrack-ng
 
-if [ -f /sys/firmware/devicetree/base/model ] && [ "$1" -eq "-lipo" ]; then
+if [ -f /sys/firmware/devicetree/base/model ] && [ "$1" == "-lipo" ]; then
   curl https://raw.githubusercontent.com/nbdy/clean-shutdown/master/setup.sh | bash
 fi
