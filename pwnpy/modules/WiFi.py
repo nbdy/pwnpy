31d8a28f9bef8d75ac61600a33a981d2912b8c41
Pascal Eberlein <pascal@eberlein.io>
2021-04-05 16:53:36 +0200

    split the big function up

--- pwnpy/modules/WiFi.py
+++ pwnpy/modules/WiFi.py
@@ -35,37 +35,51 @@ class WiFi(Module):
                 o[k] = n[k]
         return o
 
+    def update_shared_data(self, i):
+        if "ssid" in i.keys() and "crypto" in i.keys():
+            s = i["ssid"]
+            e = i["crypto"].lower()
+            if s not in self.seen_ssids:
+                self.seen_ssids.append(s)
+                log.debug("Encryption: {0}", e)
+                if "open" in e or "opn" in e:
+                    self.shared_data["opn"] += 1
+                elif "wep" in e:
+                    self.shared_data["wep"] += 1
+                elif "wpa" in e:
+                    self.shared_data["wpa"] += 1
+
+    def dot11_beacon_check(self, i, pkt):
+        if Dot11Beacon in pkt:
+            ns = pkt[Dot11Beacon].network_stats()
+            i = self.set_or_not(i, ns, "ssid")
+            i = self.set_or_not(i, ns, "rates")
+            i = self.set_or_not(i, ns, "channel")
+            i = self.set_or_not(i, ns, "country")
+            i = self.set_or_not(i, ns, "crypto")
+        return i
+
+    def add_coordinate(self, i):
+        if "GPS" in self.mgr.shared_data.keys() and self.mgr.shared_data["GPS"]["id"] is not None:
+            i["coordinates"] = self.mgr.shared_data["GPS"]["id"]
+        return i
+
+    def process_dot11(self, pkt):
+        i = {
+            "addr1": pkt[Dot11].addr1,
+            "addr2": pkt[Dot11].addr2,
+            "addr3": pkt[Dot11].addr3,
+            "addr4": pkt[Dot11].addr4,
+            "rssi": pkt[RadioTap].dBm_AntSignal
+        }
+        i = self.add_coordinate(i)
+        i = self.dot11_beacon_check(i, pkt)
+        self.update_shared_data(i)
+        self.save(i)
+
     def _callback(self, pkt):
         if Dot11 in pkt:
-            i = {
-                "addr1": pkt[Dot11].addr1,
-                "addr2": pkt[Dot11].addr2,
-                "addr3": pkt[Dot11].addr3,
-                "addr4": pkt[Dot11].addr4,
-                "rssi": pkt[RadioTap].dBm_AntSignal
-            }
-            if Dot11Beacon in pkt:
-                ns = pkt[Dot11Beacon].network_stats()
-                i = self.set_or_not(i, ns, "ssid")
-                i = self.set_or_not(i, ns, "rates")
-                i = self.set_or_not(i, ns, "channel")
-                i = self.set_or_not(i, ns, "country")
-                i = self.set_or_not(i, ns, "crypto")
-
-            if "ssid" in i.keys() and "crypto" in i.keys():
-                s = i["ssid"]
-                e = i["crypto"].lower()
-                if s not in self.seen_ssids:
-                    self.seen_ssids.append(s)
-                    log.debug("Encryption: {0}", e)
-                    if "open" in e or "opn" in e:
-                        self.shared_data["opn"] += 1
-                    elif "wep" in e:
-                        self.shared_data["wep"] += 1
-                    elif "wpa" in e:
-                        self.shared_data["wpa"] += 1
-
-            self.save(i)
+            self.process_dot11(pkt)
 
     def on_start(self):
         if geteuid() == 0:
