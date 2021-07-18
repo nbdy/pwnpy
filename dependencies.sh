9df02ed2818ce46e0cb510d62cdfc37692cb7b9a
Pascal Eberlein <pascal@eberlein.io>
2021-07-18 20:16:00 +0000

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Installing apt dependencies"
+sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install -y python3 python3-dev python3-pip cython3 gpsd gpsd-clients libgps-dev \
                         libpcap-dev libopenjp2-tools aircrack-ng libbluetooth-dev libjpeg-dev \
-                        libglib2.0-dev
+                        libglib2.0-dev git
 
 pip3 install -r requirements.txt --upgrade
 
